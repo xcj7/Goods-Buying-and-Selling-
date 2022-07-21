853b248c50a8ed25de83c0efabb5778a4e73a2c2
xcj7 <xcjtushar@gmial.com>
2022-07-21 14:45:51 +0600

    final migration

---
+++ app/Http/Controllers/AllUserController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\AllUser;
+use App\Http\Requests\StoreAllUserRequest;
+use App\Http\Requests\UpdateAllUserRequest;
+
+class AllUserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreAllUserRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreAllUserRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\AllUser  $allUser
+     * @return \Illuminate\Http\Response
+     */
+    public function show(AllUser $allUser)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\AllUser  $allUser
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(AllUser $allUser)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateAllUserRequest  $request
+     * @param  \App\Models\AllUser  $allUser
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateAllUserRequest $request, AllUser $allUser)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\AllUser  $allUser
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(AllUser $allUser)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/EmailController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Email;
+use App\Http\Requests\StoreEmailRequest;
+use App\Http\Requests\UpdateEmailRequest;
+
+class EmailController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreEmailRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreEmailRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Email  $email
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Email $email)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Email  $email
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Email $email)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateEmailRequest  $request
+     * @param  \App\Models\Email  $email
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateEmailRequest $request, Email $email)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Email  $email
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Email $email)
+    {
+        //
+    }
+}
